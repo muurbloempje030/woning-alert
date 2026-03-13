e18ba16773e3b515c7fed03a2fd1211d7e3d8946
muurbloempje030 <katinka_blom@hotmail.com>
2026-03-13 22:11:20 +0100

    Update check_woningen.py

--- check_woningen.py
+++ check_woningen.py
@@ -4,34 +4,34 @@ import smtplib
 from email.mime.text import MIMEText
 import os
 
+# URL van de woningpagina
 URL = "https://your-house.nl/woning-huren"
 
-EMAIL_FROM = os.environ["EMAIL_FROM"]
-EMAIL_PASS = os.environ["EMAIL_PASS"]
-EMAIL_TO = "katinka_blom@hotmail.com"
+# E-mailgegevens via environment variables
+EMAIL_FROM = os.environ["EMAIL_FROM"]  # ← GitHub secret
+EMAIL_PASS = os.environ["EMAIL_PASS"]  # ← GitHub secret
+EMAIL_TO = "katinka_blom@hotmail.com"  # ← jouw e-mailadres
 
+# Website ophalen
 r = requests.get(URL)
 soup = BeautifulSoup(r.text, "html.parser")
 
+# Zoek links naar woningen
 links = []
-
 for a in soup.find_all("a", href=True):
     if "/woning/" in a["href"]:
         links.append("https://your-house.nl" + a["href"])
 
+# Alleen mailen als er links zijn
 if links:
-
-    msg = "Nieuwe woningen gevonden:\n\n"
-
-    for l in links[:5]:
-        msg += l + "\n"
-
-    message = MIMEText(msg)
+    msg_content = "Nieuwe woningen gevonden:\n\n" + "\n".join(links[:5])
+    message = MIMEText(msg_content)
     message["Subject"] = "Nieuwe woning op Your-House"
     message["From"] = EMAIL_FROM
     message["To"] = EMAIL_TO
 
+    # Outlook SMTP verbinding
     with smtplib.SMTP("smtp.office365.com", 587) as server:
-        server.starttls()
-        server.login(EMAIL_FROM, EMAIL_PASS)
+        server.starttls()  # ← versleuteling
+        server.login(EMAIL_FROM, EMAIL_PASS)  # ← login met secrets
         server.send_message(message)
